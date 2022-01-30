24c5118072b9f4146472379ec2c5d3f5b54fe9c5
Boromir-the-Brave <zanegloh@gmail.com>
2022-01-29 19:39:42 -0800

    more in depth skeleton for locations/items

---
+++ things.py
@@ -0,0 +1,28 @@
+"""
+Includes Locations, Rooms, Items
+"""
+
+
+class Location():
+
+    def __init__(self, id):
+        self.id = id
+        self.desc = ''
+        self.connnected_to = []
+        self.rooms = []
+
+
+class Room():
+
+    def __init__(self, id):
+        self.id = id
+        self.desc = ''
+        self.connnected_to = []
+        self.items = []
+
+
+class Item():
+
+    def __init__(self, id):
+        self.id = id
+        self.desc = ''
