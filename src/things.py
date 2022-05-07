364b4f0bd81096b08ac45d9ed33b72e7edfd81c8
Boromir-the-Brave <zanegloh@gmail.com>
2022-05-06 19:54:13 -0700

    added class methods for exploration and inspect

--- src/things.py
+++ src/things.py
@@ -3,13 +3,13 @@ Includes Locations, Rooms, Items
 """
 
 
-class Location():
+# class Location():
 
-    def __init__(self, id):
-        self.id = id
-        self.desc = ''
-        self.connnected_to = []
-        self.rooms = []
+#     def __init__(self, id):
+#         self.id = id
+#         self.desc = ''
+#         self.connnected_to = []
+#         self.rooms = []
 
 
 class Room():
@@ -17,8 +17,15 @@ class Room():
     def __init__(self, id):
         self.id = id
         self.desc = ''
+        self.hidden_desc = ''
         self.connnected_to = []
         self.items = []
+    
+    def connect(self, other):
+        self.connected_to.append(other)
+    
+    def explore(self):
+        return self.desc
 
 
 class Item():
@@ -26,5 +33,14 @@ class Item():
     def __init__(self, id):
         self.id = id
         self.desc = ''
+        self.hidden_desc = ''
         self.pickupable = False
-        
+    
+    def insert(self, room):
+        room.items.append(self)
+    
+    def inspect(self):
+        return self.desc
+    
+    def inspect_closely(self):
+        return self.hidden_desc
