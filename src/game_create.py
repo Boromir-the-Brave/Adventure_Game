dd622c82cd6d8d62a16e9375e6d839f3964e4a60
Boromir-the-Brave <zanegloh@gmail.com>
2022-05-06 19:54:51 -0700

    started process for creating game and generating items, still need to fully flesh out the rooms of the game before this can be fully implemented

---
+++ src/game_create.py
@@ -0,0 +1,17 @@
+from things import Room, Item
+
+def build_rooms():
+    print("Building world...", end="")
+
+    house_front_yard = Room("house_front_yard")
+
+
+    print(" done.")
+    return
+
+def generate_items():
+    print("Generating items...", end="")
+
+
+    print(" done.")
+    return
